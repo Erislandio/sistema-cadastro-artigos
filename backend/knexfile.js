b4f0ff2804e97cbdd9ffec1441ca5059a3696452
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-18 23:44:15 -0200

    Ajustes das migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
     client: 'postgresql',
     connection: {
-        database: 'knowlegde',
+        database: 'knowledge',
         user: 'postgres',
         password: 'root'
     },
