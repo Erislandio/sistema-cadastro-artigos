3f626dc51e978a32301bf0c90bb481fc9d510c8a
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-18 23:37:30 -0200

    Ajustes das migrations e criacao da migrations para postgres

--- backend/migrations/20181218232106_create_table_articles.js
+++ backend/migrations/20181218232106_create_table_articles.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = function (knex, Promise) {
+    knex.schema, createTable('articles', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('discription', 1000).notNull()
+        table.string('imageUrl', 1000)
+        table.binary('content').notNull()
+        table.integer('userId').references('id')
+            .inTable('users')
+    })
 
-exports.down = function(knex, Promise) {
-  
-};
+    exports.down = function (knex, Promise) {
+        knex.schema.dropTable('articles')
+    };
+}
