52c79636e6159e1303b42473e643708e8ba1509d
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-22 11:35:12 -0200

    Adição das rotas para a categoria

--- backend/api/category.js
+++ backend/api/category.js
@@ -1,14 +1,19 @@
 module.exports = app => {
-    const { exitsOrError, notExistsOrError } = app.api.validation
+    const { existsOrError, notExistsOrError } = app.api.validation
 
     const save = (req, res) => {
-        const category = { ...req.body }
+        const category = {
+            id: req.body.id,
+            name: req.body.name,
+            parentId: req.body.parentId
+        }
+
         if (req.params.id) category.id = req.params.id
 
         try {
-            exitsOrError(category.name, "Nome não informado")
+            existsOrError(category.name, 'Nome não informado')
         } catch (msg) {
-            return res.status(400).send()
+            return res.status(400).send(msg)
         }
 
         if (category.id) {
@@ -16,36 +21,78 @@ module.exports = app => {
                 .update(category)
                 .where({ id: category.id })
                 .then(_ => res.status(204).send())
-                .catch(err => res.status(500).send())
+                .catch(err => res.status(500).send(err))
         } else {
             app.db('categories')
                 .insert(category)
                 .then(_ => res.status(204).send())
-                .catch(err => res.status(500).send())
+                .catch(err => res.status(500).send(err))
         }
     }
 
     const remove = async (req, res) => {
         try {
-            // ! verifica se possui subcategoria
-            exitsOrError(req.params.id, "Código da categoria não foi informado.")
+            existsOrError(req.params.id, 'Código da Categoria não informado.')
+
             const subcategory = await app.db('categories')
                 .where({ parentId: req.params.id })
-            // ! Verifica artigos 
-            notExistsOrError(subcategory, "Categoria possui subcategoria")
-            const articles = await app.db('categories')
+            notExistsOrError(subcategory, 'Categoria possui subcategorias.')
+
+            const articles = await app.db('articles')
                 .where({ categoryId: req.params.id })
+            notExistsOrError(articles, 'Categoria possui artigos.')
 
-            notExistsOrError(articles, "Categoria possui artigos. ")
-            // ! deleta categoria 
             const rowsDeleted = await app.db('categories')
-                .where({ id: req.params.id }).del();
-            exitsOrError(rowsDeleted, "Categoria não foi encontrada")
-            // * envia o status
-            res.status(204).send()
+                .where({ id: req.params.id }).del()
+            existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
 
+            res.status(204).send()
         } catch (msg) {
             res.status(400).send(msg)
         }
     }
+
+    const withPath = categories => {
+        const getParent = (categories, parentId) => {
+            const parent = categories.filter(parent => parent.id === parentId)
+            return parent.length ? parent[0] : null
+        }
+
+        const categoriesWithPath = categories.map(category => {
+            let path = category.name
+            let parent = getParent(categories, category.parentId)
+
+            while (parent) {
+                path = `${parent.name} > ${path}`
+                parent = getParent(categories, parent.parentId)
+            }
+
+            return { ...category, path }
+        })
+
+        categoriesWithPath.sort((a, b) => {
+            if (a.path < b.path) return -1
+            if (a.path > b.path) return 1
+            return 0
+        })
+
+        return categoriesWithPath
+    }
+
+    const get = (req, res) => {
+        app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('categories')
+            .where({ id: req.params.id })
+            .first()
+            .then(category => res.json(category))
+            .catch(err => res.status(500).send(err))
+    }
+
+
+    return { save, remove, get, getById }
 }
