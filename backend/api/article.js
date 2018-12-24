bdc16dbdd0f9d3e0b7fa5d08e35eab93fcc4c022
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-24 11:47:52 -0200

    Ajustes dos nomes dos campoes do banco

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,17 +1,16 @@
 module.exports = app => {
-    const { existsOrError, notExistsOrError } = app.api.validation
+    const { existsOrError } = app.api.validation
 
     const save = (req, res) => {
         const article = { ...req.body }
         if (req.params.id) article.id = req.params.id
 
-        // * Validação dos campos
         try {
-            existsOrError(article.name, "Nome não foi definido")
-            existsOrError(article.description, "Descrição nao foi informado")
-            existsOrError(article.categoryId, "ID da Categoria não foi informado")
-            existsOrError(article.userId, "Autor não informado")
-            existsOrError(article.content, "Conteudo não foi informado")
+            existsOrError(article.name, 'Nome não informado')
+            existsOrError(article.discription, 'Descrição não informada')
+            existsOrError(article.categoryId, 'Categoria não informada')
+            existsOrError(article.userId, 'Autor não informado')
+            existsOrError(article.content, 'Conteúdo não informado')
         } catch (msg) {
             res.status(400).send(msg)
         }
@@ -34,7 +33,12 @@ module.exports = app => {
         try {
             const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id }).del()
-            notExistsOrError(rowsDeleted, "Artigo não encontrado")
+
+            try {
+                existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+            } catch (msg) {
+                return res.status(400).send(msg)
+            }
 
             res.status(204).send()
         } catch (msg) {
@@ -42,17 +46,18 @@ module.exports = app => {
         }
     }
 
-    // ! paginação
-    const limit = 10
+    const limit = 10 // usado para paginação
     const get = async (req, res) => {
         const page = req.query.page || 1
+
         const result = await app.db('articles').count('id').first()
         const count = parseInt(result.count)
 
         app.db('articles')
-            .select('id', 'name', 'descripton')
+            .select('id', 'name', 'discription')
             .limit(limit).offset(page * limit - limit)
             .then(articles => res.json({ data: articles, count, limit }))
+            .catch(err => res.status(500).send(err))
     }
 
     const getById = (req, res) => {
@@ -65,5 +70,5 @@ module.exports = app => {
             })
             .catch(err => res.status(500).send(err))
     }
-
+    return { save, remove, get, getById }
 }
