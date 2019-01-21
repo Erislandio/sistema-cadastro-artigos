b9859ba785b5f1afda3f6e9a58d960b56baaa296
Erislandio Soares <erislandiosoares21@gmail.com>
2019-01-21 19:33:15 -0200

    Ajustes no login como administrador

--- backend/api/user.js
+++ backend/api/user.js
@@ -16,6 +16,9 @@ module.exports = app => {
         const user = { ...req.body }
         if (req.params.id) user.id = req.params.id
 
+        if (!req.orinalUrl.startsWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
+
         // * validação do usuarios
         try {
             existsOrError(user.name, "Nome não informado")
